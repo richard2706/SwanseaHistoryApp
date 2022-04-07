51c47d5d20159017d96416cf27770994439ec8d3
Richard Wakeham <rwakeham27@outlook.com>
2022-04-07 02:10:35 +0100

    Add PoI data class

---
+++ app/src/main/java/com/example/swanseahistoryapp/PointOfInterest.kt
@@ -0,0 +1,7 @@
+package com.example.swanseahistoryapp
+
+import com.google.firebase.firestore.GeoPoint
+
+data class PointOfInterest(val id : String, val name : String, val address : String,
+                      val description : String, val location : GeoPoint, val imageURL : String) {
+}
