8cd9d990af936ae74dd4d9b4c32881640e638748
Richard Wakeham <rwakeham27@outlook.com>
2022-04-07 03:40:04 +0100

    Add database and read data
    Can successfully read data (added manually in Firebase console)

--- app/src/main/java/com/example/swanseahistoryapp/MapsActivity.kt
+++ app/src/main/java/com/example/swanseahistoryapp/MapsActivity.kt
@@ -10,15 +10,29 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.example.swanseahistoryapp.databinding.ActivityMapsBinding
+import com.google.firebase.firestore.GeoPoint
+import com.google.firebase.firestore.QuerySnapshot
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlin.reflect.typeOf
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     companion object {
         private val SWANSEA_LOCATION = LatLng(51.6255408,-3.9655064)
+        private const val DEFAULT_ZOOM = 11F
+        private const val POI_COLLECTION_NAME = "points_of_interest"
+        private const val NAME_FIELD = "name"
+        private const val ADDRESS_FIELD = "address"
+        private const val DESCRIPTION_FIELD = "description"
+        private const val LOCATION_FIELD = "location"
+        private const val IMAGE_URL_FIELD = "image_url"
     }
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private val db = Firebase.firestore
+    private lateinit var pois: List<PointOfInterest>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +44,27 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        db.collection(POI_COLLECTION_NAME).get()
+            .addOnSuccessListener { result -> parsePois(result) }
+            .addOnFailureListener { exception -> Log.w("firebase-log", exception) }
+    }
+
+    /**
+     * Stores the data from the database in a list of PointOfInterest objects.
+     */
+    fun parsePois(result : QuerySnapshot) {
+        var poiList  = mutableListOf<PointOfInterest>()
+        for (document in result) {
+            val id = document.id
+            val name = document.getString(NAME_FIELD)
+            val address = document.getString(ADDRESS_FIELD)
+            val description = document.getString(DESCRIPTION_FIELD)
+            val location = document.getGeoPoint(LOCATION_FIELD)
+            val imageURL = document.getString(IMAGE_URL_FIELD)
+            poiList.add(PointOfInterest(id, name, address, description, location, imageURL))
+        }
+        pois = poiList
     }
 
     /**
@@ -38,6 +73,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SWANSEA_LOCATION, 11F))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SWANSEA_LOCATION, DEFAULT_ZOOM))
     }
 }
--- app/src/main/java/com/example/swanseahistoryapp/PointOfInterest.kt
+++ app/src/main/java/com/example/swanseahistoryapp/PointOfInterest.kt
@@ -2,6 +2,6 @@ package com.example.swanseahistoryapp
 
 import com.google.firebase.firestore.GeoPoint
 
-data class PointOfInterest(val id : String, val name : String, val address : String,
-                      val description : String, val location : GeoPoint, val imageURL : String) {
+data class PointOfInterest(val id : String, val name : String?, val address : String?,
+                      val description : String?, val location : GeoPoint?, val imageURL : String?) {
 }
