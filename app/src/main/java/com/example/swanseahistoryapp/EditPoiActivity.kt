4474d70e283745a7b8579cccfc428dd26e187c00
Richard Wakeham <rwakeham27@outlook.com>
2022-04-15 03:58:36 +0100

    Pick location on map

--- app/src/main/java/com/example/swanseahistoryapp/EditPoiActivity.kt
+++ app/src/main/java/com/example/swanseahistoryapp/EditPoiActivity.kt
@@ -1,14 +1,45 @@
 package com.example.swanseahistoryapp
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.EditText
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.widget.Toolbar
+import com.google.android.gms.maps.model.LatLng
+import com.google.firebase.firestore.GeoPoint
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class EditPoiActivity : AppCompatActivity() {
+    companion object {
+        private const val POI_COLLECTION = "points_of_interest"
+        private const val POI_NAME_FIELD = "name"
+        private const val POI_ADDRESS_FIELD = "address"
+        private const val POI_DESCRIPTION_FIELD = "description"
+        private const val POI_LOCATION_FIELD = "location"
+        private const val POI_IMAGE_URL_FIELD = "image_url"
+    }
+
+    private val db = Firebase.firestore
     private var newPoi = true
+
+    private var pickedLocation : LatLng? = null
+
+    // Saves the picked location from the location picker activity
+    private val locationPickerForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            result : ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                pickedLocation = result.data?.getParcelableExtra("pickedLocation")
+            }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,10 +78,28 @@ class EditPoiActivity : AppCompatActivity() {
     }
 
     /**
+     * Opens the location picker activity when the location picker button is clicked.
+     */
+    fun onChooseLocationButtonClick(view : View) {
+        locationPickerForResult.launch(Intent(this, LocationPickerActivity::class.java))
+    }
+
+    /**
      * Save the new PoI to the database.
      */
     private fun saveNewPoi() {
+        // upload image to firebase storage, get url
 
+        val location = if (pickedLocation != null)
+            GeoPoint(pickedLocation!!.latitude, pickedLocation!!.longitude) else null
+//        val data = hashMapOf(
+//            "name" to findViewById<EditText>(R.id.field_poi_name).text.toString(),
+//            "address" to findViewById<EditText>(R.id.field_poi_address).text.toString(),
+//            "location" to location,
+//            "image_url" to ,
+//            "description" to findViewById<EditText>(R.id.field_poi_description).text.toString()
+        )
+        db.collection(POI_COLLECTION).document().set
     }
 
     /**
---
+++ app/src/main/java/com/example/swanseahistoryapp/LocationPickerActivity.kt
@@ -0,0 +1,68 @@
+package com.example.swanseahistoryapp
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.swanseahistoryapp.databinding.ActivityLocationPickerBinding
+
+class LocationPickerActivity : AppCompatActivity(), OnMapReadyCallback,
+    GoogleMap.OnMapClickListener {
+
+    companion object {
+        private val SWANSEA_LOCATION = LatLng(51.6255408,-3.9655064)
+        private const val DEFAULT_ZOOM = 11F
+    }
+
+    private lateinit var map: GoogleMap
+    private lateinit var binding: ActivityLocationPickerBinding
+    private lateinit var pickedLocation : LatLng
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLocationPickerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(SWANSEA_LOCATION, DEFAULT_ZOOM))
+        map.setOnMapClickListener(this)
+    }
+
+    /**
+     * Return the clicked location to the previous activity.
+     */
+    override fun onMapClick(location: LatLng) {
+        Log.i("add-poi-debug", "onMapClick")
+        pickedLocation = location
+        finish()
+    }
+
+    /**
+     * Pass the picked location back to the previous activity.
+     */
+    override fun finish() {
+        val data = Intent()
+        data.putExtra("pickedLocation", pickedLocation)
+        setResult(Activity.RESULT_OK, data)
+        super.finish()
+    }
+}
